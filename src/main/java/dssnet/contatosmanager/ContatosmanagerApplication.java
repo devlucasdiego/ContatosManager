eeb8145368b82e0fa5586dcdcd09bfb647e1b39d
Lucas Diego <luucasdiego_12@hotmail.com>
2022-01-14 21:21:17 -0400

    Projeto Iniciado

---
+++ src/main/java/dssnet/contatosmanager/ContatosmanagerApplication.java
@@ -0,0 +1,13 @@
+package dssnet.contatosmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dssnet/contatosmanager/ContatosmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dssnet.contatosmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatosmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
