f96c6cd47896fbdc000b13d490323655aae9009b
Lucas Diego <luucasdiego_12@hotmail.com>
2022-01-15 22:46:14 -0400

    Classe Contato e Configs de conexão MySQL

--- src/main/java/dssnet/contatosmanager/ContatosmanagerApplication.java
+++ src/main/java/dssnet/contatosmanager/ContatosmanagerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContatosmanagerApplication {
+public class git ContatosmanagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ContatosmanagerApplication.class, args);
